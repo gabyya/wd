26fc858e4a2af2cc4e8b4010d8437b1ccf504169
gabyya <31488533+gabyya@users.noreply.github.com>
2017-11-24 12:17:38 -0800

    edits

--- app/public/wp-content/themes/fourohfive-master/template-parts/content-scroller.php
+++ app/public/wp-content/themes/fourohfive-master/template-parts/content-scroller.php
@@ -1,15 +1,37 @@
 
-<div class="image_wrapper">
-    <?php
-    	the_post_thumbnail('full', ['class' => 'img-fluid']);
-    ?>
+<div id="wd">
+    <div class="image_wrapper">
+        <!-- nav -->
+        <div class= "navbar d-flex justify-content-end">
+                <ul class="nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#wd">WD</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#product">Product</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#team">Team</a>
+                  </li>
+                   <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+                 </ul>
+        </div>
 
-    <div class="flex">
-        <h2 class="image_description">
-            <?php the_field ('featured_text') ?>
-        </h2>
+
+        <?php
+        	the_post_thumbnail('full', ['class' => 'img-fluid']);
+        ?>
+
+            <div class="flex">
+                <h2 class="image_description">
+                    <?php the_field ('featured_text') ?>
+                </h2>
+            </div>
+        
+
     </div>
-
 </div>
 
 
@@ -29,72 +51,72 @@
 </div>
 
 
-<div class="about_product">
-    <h2>
+<div id= "product">
+    <h2 class="download">
     <?php the_field ('second_title') ?>
     </h2>
 
-    <div class="">
-    <img class="grid-one-third" src="<?php the_field ('product_image');?>" alt="">
-    </div>
-
-    <div>
-        <p>
-        <?php the_field ('second_description') ?>
+    <div class="about_product">
+        <img class="grid-one-third" src="<?php the_field ('product_image');?>" alt="">
+            
+        <p class="sensor">
+            <?php the_field ('second_description') ?>
         </p>
+        
     </div>
 </div>
 
 
 <img class="img-fluid" src="<?php the_field ('second_break_image');?>" alt="">
 
-
-<div class="team">
-    <h2>
-    <?php the_field ('third_title') ?>
-    </h2>
-
-
-    <p>
-    <?php the_field ('third_description') ?>
-    </p>
-</div>
+<div id="team">
+    <div class="team">
+        <h2>
+        <?php the_field ('third_title') ?>
+        </h2>
 
 
+        <p>
+        <?php the_field ('third_description') ?>
+        </p>
+    </div>
 
-<div class="flex-md">
-    <?php
 
-    // check if the repeater field has rows of data
-    if( have_rows('section_grid') ):
 
-        // loop through the rows of data
-        while ( have_rows('section_grid') ) : the_row();
-            ?>
+    <div class="grid-thirds-md">
+        <?php
 
-            <div class="deets text-center">
-                <img class= "person img-fluid" src="<?php the_sub_field ('grid_image');?>" alt="">
+        // check if the repeater field has rows of data
+        if( have_rows('section_grid') ):
 
-                    <div class="py">
-                        <h3 class= "person_description">
-                            <?php the_sub_field ('grid_title') ?>
-                        </h3>
+            // loop through the rows of data
+            while ( have_rows('section_grid') ) : the_row();
+                ?>
 
-                        <p class="person_image">
-                            <?php the_sub_field ('grid_description') ?>
-                        </p>
-                    </div>
+                <div class="deets text-center">
+                    <img class= "person img-fluid" src="<?php the_sub_field ('grid_image');?>" alt="">
 
-            </div>
+                        <div class="py">
+                            <h3 class= "person_title">
+                                <?php the_sub_field ('grid_title') ?>
+                            </h3>
+
+                            <p class="person_description">
+                                <?php the_sub_field ('grid_description') ?>
+                            </p>
+                        </div>
+
+                </div>
+
+                
+            <?php endwhile; ?>
+
+            <?php else : ?>
 
             
-        <?php endwhile; ?>
 
-        <?php else : ?>
-
-        
-
-    <?php endif; ?>
+        <?php endif; ?>
+    </div>
 </div>
 
 
@@ -102,38 +124,40 @@
 
 
 
-<div class="container">
+<div id="contact">
+    <div class="container">
 
-        
-            <h3>
-                <?php the_field ('contact') ?>
-            </h3>
+            
+                <h3>
+                    <?php the_field ('contact') ?>
+                </h3>
 
-            <p>
-                <?php the_field ('contact_description') ?>
-            </p>
-        
-            <div class="grid-two-thirds">
-                <div class="content">
-                    <?php the_content();?>
+                <p class="contact_description">
+                    <?php the_field ('contact_description') ?>
+                </p>
+            
+                <div class="grid-two-thirds">
+                    <div class="content">
+                        <?php the_content();?>
+                    </div>
+
+                    <div class="connect">
+                        <h3>
+                            <?php the_field ('connect') ?>
+                        </h3>
+
+                        <p>
+                            <?php the_field ('connect_description') ?>
+                        </p>
+                        
+                        <p>
+                            <?php the_field ('location') ?>
+                        </p>
+                    </div>
                 </div>
 
-                <div class="connect">
-                    <h3>
-                        <?php the_field ('connect') ?>
-                    </h3>
 
-                    <p>
-                        <?php the_field ('connect_description') ?>
-                    </p>
-                    
-                    <p>
-                        <?php the_field ('location') ?>
-                    </p>
-                </div>
-            </div>
-
-
+    </div>
 </div>
 
 
